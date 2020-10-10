0b96ba09fea423a88ebe5b7db31c51dadeab3609
ShaninIvan <shaninivan1993@gmail.com>
2020-10-10 17:24:24 +0300

    complite pixel perfect 1920

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-// Эта функция при загрузке страницы будет проверять список скиллов и менять прогресс из значения в span.percent
+
 window.onload = () =>{
-    [...document.querySelectorAll('.skills-list__skill')].forEach(skill =>{
-        let percent = skill.querySelector('.percent').innerText;
+    // Эта функция при загрузке страницы будет проверять список скиллов и менять прогресс из значения в span.percent
+    [...document.querySelectorAll('.about-me__skill')].forEach(skill =>{
+        let percent = skill.querySelector('.about-me__percent').innerText;
         
         percent = (Number.isInteger(parseInt(percent))) ? parseInt(percent) : 0;
         percent = (percent>0) ? percent : 0;
         percent = (percent<100) ? percent : 100;
         
-        const level = skill.querySelector('.skills-list__skill-level');
+        const level = skill.querySelector('.about-me__skill-level');
         level.style.width = `${percent}%`;
-    })
-}
+    });
+
+    //Эта функция добавит проектам "маски", которые будут появляться при наведении
+    [...document.querySelectorAll('.projects__project')].forEach(project => {
+        const mask = document.createElement('div');
+        mask.className = 'projects__mask';
+        project.append(mask);
+    });
+};
+
+document.querySelector('.contacts__submit').addEventListener('click', (event)=>{
+    event.preventDefault();
+});
